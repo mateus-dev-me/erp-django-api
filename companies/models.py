0f8cc31dc24686119c9388627dbd0039a074598a
mateus-dev-me <mateusmartinsipu2@gmail.com>
2025-01-29 00:29:04 -0300

    ✨ feat: add models Enterprise, Employee, TaskStatus and Task

---
+++ companies/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from core.utils.models import BaseModel
+
+
+class Enterprise(BaseModel):
+    company_name = models.CharField(max_length=150, default='', unique=True)
+    document = models.CharField(max_length=14, default='', unique=True)
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        app_label = 'companies'
+
+
+class Employee(BaseModel):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f'Employee(user={self.user}, enterprise={self.enterprise})'
+
+    class Meta:
+        app_label = 'companies'
+
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=155)
+    codename = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = 'companies'
+        db_table = 'companies_task_status'
+
+
+class Task(BaseModel):
+    title = models.TextField()
+    description = models.TextField(null=True)
+    due_date = models.DateTimeField(null=True)
+    status = models.ForeignKey(TaskStatus, on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        app_label = 'companies'
